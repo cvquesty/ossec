ae1d33aa1c247d48a33b9f5a8a0e537aa4e8eb73
cvquesty <questy@gmail.com>
2018-11-04 17:30:31 -0500

    adding in new code for package installation, but had to drop it for awhile.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,6 +1,25 @@
 # OSSEC Agent Installer
 class ossec::agent {
 
+  # Choose packages based on OS
+  case $facts['os']['name'] {
+    'RedHat': {
+      # Install the main Package
+      package { 'ossec-hids':
+        ensure => 'installed',
+      }
 
+      # Install the Server Component
+      package { 'ossec-hids-client':
+        ensure => 'installed',
+      }
+    }
 
+    'Ubuntu': {
+
+    }
+
+    default: {
+    }
+  }
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,13 @@
 # OSSEC Server Installer
 class ossec::server {
 
+  # Install the main Package
+  package { 'ossec-hids':
+    ensure => 'installed',
+  }
 
-
+  # Install the Server Component
+  package { 'ossec-hids-server':
+    ensure => 'installed',
+  }
 }
