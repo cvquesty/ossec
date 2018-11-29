ebe37b5ae851365c1ab546e5bc81ce20fdda07a6
cvquesty <questy@gmail.com>
2018-11-28 20:46:39 -0500

    added in some extra documentation for the server installation manifest for clarity.

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,10 @@
 # OSSEC Server Installer
 class ossec::server {
 
+  # Since the OSSEC repos are in place first, all platforms can call on
+  # Ossec via the package resource. In this way, no extra conditionals
+  # or case statements are necessary before asserting the server install
+
   # Install the main Package
   package { 'ossec-hids':
     ensure => 'installed',
