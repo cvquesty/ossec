d3afa71b15e1792dc022d155d5b500498e5b7fc1
cvquesty <questy@gmail.com>
2018-11-30 11:18:01 -0500

    ensuring the file resource is present fixes two problems... first that if it is installed, it wont try to do it again, and second that there wont be as much output.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -17,7 +17,7 @@ class ossec::repo {
         '6': {
           # Install Repo
           package { 'repofile':
-            ensure   => 'installed',
+            ensure   => 'present',
             provider => 'rpm',
             source   => 'https://updates.atomicorp.com/channels/atomic/centos/6/i386/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm',
             require  => Exec['import_key'],
@@ -27,7 +27,7 @@ class ossec::repo {
         '7': {
           # Install Repo
           package { 'repofile':
-            ensure   => 'installed',
+            ensure   => 'present',
             provider => 'rpm',
             source   => 'https://updates.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.el7.art.noarch.rpm',
             require  => Exec['import_key'],
