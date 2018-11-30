6499d5057003a8a21f25d84f6ceb5ef0cd3f8c02
cvquesty <questy@gmail.com>
2018-11-30 10:57:31 -0500

    adding in logic to only update the repos if the repo file does not exist as well as only place the repo file if its not already there. Trying to drop the overhead on the module run since most people run puppet periodically all day long. This will also reduce noise in the logs for our Splunk/Elastic friends.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -21,6 +21,7 @@ class ossec::repo {
             provider => 'rpm',
             source   => 'https://updates.atomicorp.com/channels/atomic/centos/6/i386/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm',
             require  => Exec['import_key'],
+            unless   => 'test -f /etc/yum.repos.d/atomic.repo',
           }
         }
 
@@ -31,6 +32,7 @@ class ossec::repo {
             provider => 'rpm',
             source   => 'https://updates.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.el7.art.noarch.rpm',
             require  => Exec['import_key'],
+            unless   => 'test -f /etc/yum.repos.d/atomic.repo',
           }
         }
         default: {
@@ -60,6 +62,7 @@ class ossec::repo {
             content => 'deb https://updates.atomicorp.com/channels/atomic/ubuntu trusty main',
             notify  => Exec['update_repos'],
             require => Exec['import_key'],
+            unless  => 'test -f /etc/apt/sources.list/atomic./list',
           }
         }
         '16.04': {
@@ -72,6 +75,7 @@ class ossec::repo {
             content => 'deb https://updates.atomicorp.com/channels/atomic/ubuntu xenial main',
             notify  => Exec['update_repos'],
             require => Exec['import_key'],
+            unless  => 'test -f /etc/apt/sources.list/atomic./list',
           }
         }
         '18.04': {
@@ -84,6 +88,7 @@ class ossec::repo {
             content => 'deb https://updates.atomicorp.com/channels/atomic/ubuntu bionic main',
             notify  => Exec['update_repos'],
             require => Exec['import_key'],
+            unless  => 'test -f /etc/apt/sources.list/atomic./list',
           }
         }
         default: {
