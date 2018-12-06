1993dd8fb6ea2040e2c5ffb1f918495c4859d535
cvquesty <questy@gmail.com>
2018-12-06 13:34:30 -0500

    Trying to clear the color bars to find the extra "end"

--- spec/classes/agent_spec.rb
+++ spec/classes/agent_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 describe 'ossec::agent' do
-
   context 'RedHat' do
     # Set facts for RedHat Family Test
     let :facts do
       {
-        :concat_basedir => '/foo',
-        :os             => { :family => 'RedHat', :release => { :major => '6' } },
+        concat_basedir: '/foo',
+        os:             { family: 'RedHat', release: { major: '6' } },
       }
     end
 
-    context "ossec::agent class without any parameters" do
+    context 'ossec::agent class without any parameters' do
       it { is_expected.to compile.with_all_deps }
       it do
         is_expected.to contain_class('ossec::agent')
@@ -19,12 +18,9 @@ describe 'ossec::agent' do
 
     it do
       is_expected.to contain_package('ossec-hids-agent').with(
-        :ensure => 'installed',
+        ensure: 'installed',
       )
     end
-
-    it do
-      should compile
     end
   end # End of RedHat Context
 
@@ -32,13 +28,13 @@ describe 'ossec::agent' do
     # Set facts for Debian Family Test
     let :facts do
       {
-        :concat_basedir => '/foo',
-        :os             => { :family => 'Debian', :release => { :major => '14.04' } },
+        concat_basedir:  '/foo',
+        os:              { family: 'Debian', release: { major: '14.04' } },
       }
     end
 
     # Add in ossec::repo to get the file resource we're testing for on the require
-    let (:precondition) { 'include ossec::repo' }
+    let (:pre_condition) { include ossec::repo }
 
     context "ossec::agent class without any parameters" do
       it { is_expected.to compile.with_all_deps }
@@ -51,10 +47,6 @@ describe 'ossec::agent' do
       is_expected.to contain_package('ossec-hids-agent').with(
         :ensure  => 'installed',
       ).that_requires('File[/etc/apt/sources.list.d/atomic.list]')
-    end
-
-    it do
-      should compile
     end
   end # End of Debian Context
 end # End of ossec::agent
