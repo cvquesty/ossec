f9845e661bfc9eed5a799c149356458b3356c3d1
cvquesty <questy@gmail.com>
2018-12-11 11:44:52 -0500

    carrying parameters through to the config

--- manifests/config/agent.pp
+++ manifests/config/agent.pp
@@ -1,5 +1,10 @@
 # OSSEC Agent Configuration Manifest
-class ossec::config::agent {
+class ossec::config::agent (
+  $notify_by_email,       # Whether to use email alerts or not.    (email_notification - yes/no)
+  $notification_email,    # Email address to use for notifications (email_to - email@domain.com)
+  $smtp_server,           # SMTP server to use sending above emaio (smtp_server - FQDN or IP)
+  $email_from_address,    # EMail Address to display as the sender (email_from - email@domain.com)
+){
 
   # Backup the Main Config before any configuration work
   exec { 'backup_agent_config':
